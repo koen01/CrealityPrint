d4c9fb61b2b4248ba2bb50f2637cc32dadf71faa
koen <koen@aarch64-01.fritz.box>
2025-10-04 15:08:09 +0200

    metartc:   CMAKE_ARGS       -DCMAKE_C_FLAGS=-Wno-error=incompatible-pointer-types

--- deps/metartc/metartc.cmake
+++ deps/metartc/metartc.cmake
@@ -1,5 +1,3 @@
-set(patch_command git init && ${PATCH_CMD} ${CMAKE_CURRENT_LIST_DIR}/metartc_fix_pointer.patch)
-
 if(CMAKE_SYSTEM_NAME STREQUAL "Linux")
 orcaslicer_add_cmake_project(metartc
   # GIT_REPOSITORY https://github.com/aliyun/aliyun-oss-cpp-sdk.git
@@ -7,6 +5,8 @@ orcaslicer_add_cmake_project(metartc
   URL https://github.com/CrealityOfficial/metartc/archive/refs/tags/v1.0.0.tar.gz
   URL_HASH SHA256=696780282b3a9324e87d451ee2c8889e3ebc93c45d06db46571208477bc93d9a
   #PATCH_COMMAND ${patch_command}
+  CMAKE_ARGS
+      -DCMAKE_C_FLAGS=-Wno-error=incompatible-pointer-types
 )
 
 add_dependencies(dep_metartc dep_FFmpeg)
